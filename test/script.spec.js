4b3f438128fc05c9c24aa2bcd2f2dc8db73364e4
djanuario <daniel.januario@vitta.me>
2022-07-28 11:40:18 -0300

    feat: new scenario

--- test/script.spec.js
+++ test/script.spec.js
@@ -1,7 +1,21 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { check, sleep } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '3s', target: 1 },
+    { duration: '5s', target: 2 },
+    { duration: '2s', target: 0 },
+  ],
+};
 
 export default function () {
-  http.get('https://test.k6.io');
+  const res = http.get('https://httpbin.test.k6.io/');
+  check(res, {
+    'is status 200': (r) => r.status === 200,
+    'body size is 9,591 bytes': (r) => r.body.length == 9591,
+    'verify homepage text': (r) =>
+      r.body.includes('Send email to the developer'),
+  });
   sleep(1);
 }
